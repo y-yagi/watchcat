2c456df0cfb10a19f84e3c95039055120651d187
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2023-02-19 15:55:57 +0900

    Load a specific version of library

--- lib/watchcat.rb
+++ lib/watchcat.rb
@@ -1,6 +1,11 @@
 require_relative "watchcat/version"
 require_relative "watchcat/executor"
-require "watchcat/watchcat"
+
+begin
+  require "watchcat/#{RUBY_VERSION.to_f}/watchcat"
+rescue LoadError
+  require "watchcat/watchcat"
+end
 
 module Watchcat
   class << self
