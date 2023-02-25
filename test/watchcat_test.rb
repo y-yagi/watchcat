bfc47fc9c15af316442d672ea54b5e0343d7967b
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2023-02-25 17:31:32 +0900

    Extract shared operation to functions

--- test/watchcat_test.rb
+++ test/watchcat_test.rb
@@ -5,10 +5,18 @@ require "tmpdir"
 require "fileutils"
 
 class WatchcatTest < Minitest::Test
-  def test_watch_directory_without_recursive
+  def setup
     @tmpdir = Dir.mktmpdir("watchcat")
+  end
+
+  def teardown
+    @watchcat.stop
+    FileUtils.remove_entry_secure(@tmpdir)
+  end
+
+  def test_watch_directory_without_recursive
     events = []
-    w = Watchcat.watch(@tmpdir, recursive: false) { |e| events << e }
+    @watchcat = Watchcat.watch(@tmpdir, recursive: false) { |e| events << e }
 
     sleep 0.1
     FileUtils.touch(File.join(@tmpdir, "a.txt"))
@@ -21,15 +29,11 @@ class WatchcatTest < Minitest::Test
 
     sleep 0.1
     assert_equal 5, events.count
-  ensure
-    w.stop
-    FileUtils.remove_entry_secure(@tmpdir)
   end
 
   def test_watch_directory_with_recursive
-    @tmpdir = Dir.mktmpdir("watchcat")
     events = []
-    w = Watchcat.watch(@tmpdir, recursive: true) { |e| events << e }
+    @watchcat = Watchcat.watch(@tmpdir, recursive: true) { |e| events << e }
 
     sleep 0.1
     FileUtils.touch(File.join(@tmpdir, "a.txt"))
@@ -42,16 +46,12 @@ class WatchcatTest < Minitest::Test
 
     sleep 0.1
     assert_equal 7, events.count
-  ensure
-    w.stop
-    FileUtils.remove_entry_secure(@tmpdir)
   end
 
   def test_watch_file
-    @tmpdir = Dir.mktmpdir("watchcat")
     file = FileUtils.touch(File.join(@tmpdir, "a.txt"))
     events = []
-    w = Watchcat.watch(file) { |e| events << e }
+    @watchcat = Watchcat.watch(file) { |e| events << e }
 
     sleep 0.1
     FileUtils.touch(File.join(@tmpdir, "a.txt"))
@@ -60,16 +60,12 @@ class WatchcatTest < Minitest::Test
 
     sleep 0.1
     assert_equal 1, events.count
-  ensure
-    w.stop
-    FileUtils.remove_entry_secure(@tmpdir)
   end
 
   def test_watch_directory_with_force_polling
-    @tmpdir = Dir.mktmpdir("watchcat")
     events = []
-    w = Watchcat.watch(@tmpdir, force_polling: true) { |e| events << e }
-    pid = w.instance_variable_get(:@child_pid)
+    @watchcat = Watchcat.watch(@tmpdir, force_polling: true) { |e| events << e }
+    pid = @watchcat.instance_variable_get(:@child_pid)
     sleep 0.1
     inotify_count = `cat /proc/#{pid}/fdinfo/* | grep inotify | wc -l`.to_i
 
@@ -78,8 +74,5 @@ class WatchcatTest < Minitest::Test
     FileUtils.touch(File.join(@tmpdir, "a.txt"))
     sleep 0.3
     assert_equal 1, events.count
-  ensure
-    w.stop
-    FileUtils.remove_entry_secure(@tmpdir)
   end
 end
