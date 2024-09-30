2340e9370861fe68f7d8731576ee6f8ff2778c4b
Yuji Yaginuma <yuuji.yaginuma@gmail.com>
2024-10-01 08:24:35 +0900

    Only enable retry on CI

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,9 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "watchcat"
 require "debug"
 
-require 'minitest/retry'
-Minitest::Retry.use!
+if ENV["CI"]
+  require 'minitest/retry'
+  Minitest::Retry.use!
+end
 
 require "minitest/autorun"
